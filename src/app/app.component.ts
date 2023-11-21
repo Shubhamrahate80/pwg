f8d4615a4deda1bf2cd4aec34e6561735a15ab4e
Shubhamrahate80 <shubhamrahate325@gmail.com>
2023-11-21 20:56:19 +0530

    pwg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,78 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwg';
+  passLength: number = 0;
+
+  //validChar:string='';
+
+  generatePass: string = ''
+
+  includeNum: boolean = false
+
+  includeCh: boolean = false
+
+  includeSys: boolean = false
+
+  getLength(length: string) {
+
+    console.log(length);
+
+    this.passLength = +length
+
+  }
+
+  includeNumber() {
+
+    this.includeNum = !this.includeNum
+
+  }
+
+  includeChar() {
+
+    this.includeCh = !this.includeCh
+
+  }
+
+  includeSymbol() {
+
+    this.includeSys = !this.includeSys
+
+  }
+
+  genPass() {
+
+    this.generatePass = ''
+
+    let validChar = '';
+
+    if (this.includeNum) {
+
+      validChar += '1234567890'
+
+    }
+
+    if (this.includeCh) {
+
+      validChar += 'qwertyuiopkjhgfdszxcvbnm'
+
+    }
+
+    if (this.includeSys) {
+
+      validChar += '!@#$)(*&^%$'
+
+    }
+
+    console.log(validChar);
+
+    for (var s = 0; s < this.passLength; s++) {
+
+      this.generatePass = this.generatePass + validChar.charAt(Math.round(Math.random() * validChar.length));
+
+    }
+
+  }
+
+
+
 }
